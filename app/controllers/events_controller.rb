3008420e947b19d003154fbce0cffadf5a1074ff
Nicholas Utikal <nicholasutikal@gmail.com>
2018-03-05 17:02:02 +0100

    initial commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,23 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Events.all
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+
+  end
+
+  def create
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show] do
+    resources :events, only: [:index, :show, :new, :create] do
+      resources :requests, only: [:new, :create]
+      resources :invitations, only: [:new, :create]
+    end
+  end
+
 end
