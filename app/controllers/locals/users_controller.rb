d91bfac56913e84ed4853406bd480bf6141fadd6
Nicholas Utikal <nicholasutikal@gmail.com>
2018-04-16 14:10:30 +0200

    working on several things, like different markers

--- app/controllers/locals/users_controller.rb
+++ app/controllers/locals/users_controller.rb
@@ -1,21 +1,29 @@
 class Locals::UsersController < ApplicationController
 
-    def index
-     @users = policy_scope(User).order(created_at: :desc)
-     @event = Event.find(params[:event_id])
-     @users = User.where.not(latitude: nil, longitude: nil)
-     @markers = @users.map do |user|
-      {
-        lat: user.latitude,
+  def index
+   @users = policy_scope(User).order(created_at: :desc)
+   @users = User.where.not(latitude: nil, longitude: nil)
+   @user_markers = @users.map do |user|
+    {
+      lat: user.latitude,
         lng: user.longitude#,
           # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+        }
+      end
+
+      @event = Event.find(params[:event_id])
+      @events = Event.where.not(latitude: nil, longitude: nil)
+      @markers = @events.map do |event|
+        {
+          lat: event.latitude,
+          lng: event.longitude
         }
       end
     end
 
     def show
-    @event = Event.find(params[:event_id])
-    @user = User.find(params[:id])
+      @event = Event.find(params[:event_id])
+      @user = User.find(params[:id])
     @request = @event.requests.find_by_user_id(@user.id) #instance or nil
     authorize @user
   end
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,8 +1,5 @@
 import GMaps from 'gmaps/gmaps';
 
-const mapElement = document.getElementById('map');
-const markers = JSON.parse(mapElement.dataset.markers);
-
 var icon = {
     url: "https://image.flaticon.com/icons/svg/802/802905.svg", // url
     // url: "https://image.flaticon.com/icons/svg/10/10522.svg",
@@ -10,6 +7,18 @@ var icon = {
     origin: new google.maps.Point(0,0), // origin
     anchor: new google.maps.Point(0, 0) // anchor
   };
+
+   var user_icon = {
+    // url: "https://image.flaticon.com/icons/svg/603/603401.svg", // url
+    url: "https://image.flaticon.com/icons/svg/10/10522.svg",
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+  };
+
+const mapElement = document.getElementById('map');
+const markers = JSON.parse(mapElement.dataset.markers);
+const user_markers = JSON.parse(mapElement.dataset.user_markers)
 
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
@@ -20,7 +29,7 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
       lat: marker.lat,
       icon: icon
     })
-  })
+  });
 
   if (markers.length === 0) {
     map.setZoom(2);
@@ -31,5 +40,99 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   } else {
     map.fitLatLngBounds(markers);
   }
+
+ console.log(user_markers);
+  user_markers.forEach(function(marker) {
+    map.addMarker({
+      lng: marker.lng,
+      lat: marker.lat,
+      icon: user_icon
+    })
+  });
+
+
+  if (user_markers.length === 0) {
+    map.setZoom(2);
+  } else if (user_markers.length === 1) {
+    map.setCenter(user_markers[0].lat, user_markers[0].lng);
+    //console.log(map.setZoom(14));
+    map.setZoom(14 );
+  } else {
+    map.fitLatLngBounds(user_markers);
+  }
 }
+
+
+
+
+
+
+// var icon = {
+//     url: "https://image.flaticon.com/icons/svg/603/603401.svg", // url
+//     // url: "https://image.flaticon.com/icons/svg/10/10522.svg",
+//     scaledSize: new google.maps.Size(40, 40), // scaled size
+//     origin: new google.maps.Point(0,0), // origin
+//     anchor: new google.maps.Point(0, 0) // anchor
+//   };
+
+//   var user_icon = {
+//     // url: "https://image.flaticon.com/icons/svg/603/603401.svg", // url
+//     url: "https://image.flaticon.com/icons/svg/10/10522.svg",
+//     scaledSize: new google.maps.Size(40, 40), // scaled size
+//     origin: new google.maps.Point(0,0), // origin
+//     anchor: new google.maps.Point(0, 0) // anchor
+//   };
+
+//   const mapElement = document.getElementById('map');
+//   console.debug(mapElement);
+//   const markers = JSON.parse(mapElement.dataset.markers);
+//   const user_markers = JSON.parse(mapElement.dataset.user_markers)
+
+
+
+// if (mapElement) { // don't try to build a map if there's no div#map to inject in
+//   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+// console.log(markers);
+// markers.forEach(function(marker) {
+//   map.addMarker({
+//     lng: marker.lng,
+//     lat: marker.lat,
+//     icon: icon,
+//   })
+// });
+
+// if (markers.length === 0) {
+//   map.setZoom(2);
+// } else if (markers.length === 1) {
+//   map.setCenter(markers[0].lat, markers[0].lng);
+//     //console.log(map.setZoom(14));
+//     map.setZoom(14);
+//   } else {
+//     map.fitLatLngBounds(markers);
+//   }
+// // };
+
+// // if (mapElement) { // don't try to build a map if there's no div#map to inject in
+// //   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+//   console.log(user_markers);
+//   user_markers.forEach(function(marker) {
+//     map.addMarker({
+//       lng: marker.lng,
+//       lat: marker.lat,
+//       icon: user_icon,
+//     })
+//   });
+
+//   if (user_markers.length === 0) {
+//     map.setZoom(2);
+//   } else if (user_markers.length === 1) {
+//     map.setCenter(user_markers[0].lat, user_markers[0].lng);
+//     //console.log(map.setZoom(14));
+//     map.setZoom(14 );
+//   } else {
+//     map.fitLatLngBounds(user_markers);
+//   }
+// };
+
+
 
