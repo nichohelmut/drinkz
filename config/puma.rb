a4816f2a8482847180fad8c49c5f87ea983bc8a0
Nicholas Utikal <nicholasutikal@gmail.com>
2019-01-12 14:38:46 +0100

    sets again puma port to 3000

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { process.env.PORT || 3000 }
+port        ENV.fetch("PORT") { 3000 }
 bind "unix:///var/run/puma/my_app.sock"
 pidfile "/var/run/puma/my_app.sock"
 
